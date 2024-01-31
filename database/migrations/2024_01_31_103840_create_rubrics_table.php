7e5179b9c21b96f8e4ff7726febf8d3f5c672299
AlexanderKira <superair12345@gmail.com>
2024-01-31 18:22:41 +0700

    Created entities site, heading, article. Reconsidered my interest in laravel-nesteds, deleted it.

---
+++ database/migrations/2024_01_31_103840_create_rubrics_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rubrics', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->boolean('is_publish')->default(false);
+            $table->foreignId('website_id')->constrained('websites')->cascadeOnDelete();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rubrics');
+    }
+};
